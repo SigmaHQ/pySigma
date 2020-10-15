101599813b7664c9abd5d5599c1e15f686f65e67
Thomas Patzke <thomas@patzke.org>
2020-10-16 00:54:14 +0200

    Added docstrings to exception classes

--- sigma/exceptions.py
+++ sigma/exceptions.py
@@ -1,32 +1,43 @@
 class SigmaError(ValueError):
+    """Generic Sigma error and super-class of all Sigma exceptions"""
     pass
 
 class SigmaLogsourceError(SigmaError):
+    """Error in Sigma rule logosurce specification"""
     pass
 
 class SigmaDetectionError(SigmaError):
+    """Error in Sigma rule detection"""
     pass
 
 class SigmaConditionError(SigmaError):
+    """Error in Sigma rule condition"""
     pass
 
 class SigmaIdentifierError(SigmaError):
+    """Error in Sigma rule identifier"""
     pass
 
 class SigmaDateError(SigmaError):
+    """Error in Sigma rule date"""
     pass
 
 class SigmaStatusError(SigmaError):
+    """Error in Sigma rule status"""
     pass
 
 class SigmaLevelError(SigmaError):
+    """Error in Sigma rule level"""
     pass
 
 class SigmaModifierError(SigmaError):
+    """Error in Sigma rule value modifier specification"""
     pass
 
 class SigmaValueError(SigmaError):
+    """Error in Sigma rule value"""
     pass
 
 class SigmaRegularExpressionError(SigmaValueError):
+    """Error in regular expression contained in Sigma rule"""
     pass

94ffa8c08f6002ce79887be68d52baa9fec9ca7c
Thomas Patzke <thomas@patzke.org>
2021-08-09 22:55:11 +0200

    Variable coverage badge colors

--- print-coverage.py
+++ print-coverage.py
@@ -4,4 +4,12 @@ import xml.etree.ElementTree as et
 tree = et.parse("cov.xml")
 root = tree.getroot()
 coverage = float(root.attrib["line-rate"]) * 100
-print(f"{coverage:.3}%")
+print(f"COVERAGE={coverage:.3}%")
+if coverage >= 95.0:
+    print("COVERAGE_COLOR=green")
+elif coverage >= 90.0:
+    print("COVERAGE_COLOR=yellow")
+elif coverage >= 85.0:
+    print("COVERAGE_COLOR=orange")
+else:
+    print("COVERAGE_COLOR=red")

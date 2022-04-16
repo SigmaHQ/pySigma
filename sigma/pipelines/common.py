239291784179be9db3a4976a6c9fcfa1216b96ed
Thomas Patzke <thomas@patzke.org>
2022-04-16 23:24:43 +0200

    Added common log source definitions

--- sigma/pipelines/common.py
+++ sigma/pipelines/common.py
@@ -12,9 +12,39 @@ def logsource_windows_process_creation() -> LogsourceCondition:
         product="windows",
     )
 
+def logsource_windows_registry_add():
+    return LogsourceCondition(
+        category="registry_add",
+        product="windows",
+    )
+
+def logsource_windows_registry_delete():
+    return LogsourceCondition(
+        category="registry_delete",
+        product="windows",
+    )
+
+def logsource_windows_registry_event():
+    return LogsourceCondition(
+        category="registry_event",
+        product="windows",
+    )
+
+def logsource_windows_registry_set():
+    return LogsourceCondition(
+        category="registry_set",
+        product="windows",
+    )
+
 def logsource_windows_file_change() -> LogsourceCondition:
     return LogsourceCondition(
         category="file_change",
+        product="windows",
+    )
+
+def logsource_windows_file_event():
+    return LogsourceCondition(
+        category="file_event",
         product="windows",
     )
 
@@ -30,14 +60,20 @@ def logsource_windows_network_connection_initiated(initiated : bool) -> RuleCont
         value="true" if initiated else "false",
     )
 
-def logsource_linux_network_connection() -> LogsourceCondition:
-    return LogsourceCondition(
-        category="network_connection",
-        product="linux",
-    )
-
 def logsource_windows_dns_query() -> LogsourceCondition:
     return LogsourceCondition(
         category="dns_query",
         product="windows",
+    )
+
+def logsource_linux_process_creation():
+    return LogsourceCondition(
+        category="process_creation",
+        product="linux",
+    )
+
+def logsource_linux_network_connection() -> LogsourceCondition:
+    return LogsourceCondition(
+        category="network_connection",
+        product="linux",
     )

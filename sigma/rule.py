c6bb431a054286a008be57e558e3a0f9cbe1fc4a
Thomas Patzke <thomas@patzke.org>
2022-08-03 13:46:35 +0200

    Ignoring deprecated timeframe detection item

--- sigma/rule.py
+++ sigma/rule.py
@@ -476,7 +476,7 @@ class SigmaDetections:
                 detections={
                     name: SigmaDetection.from_definition(definition, source)
                     for name, definition in detections.items()
-                    if name != "condition"
+                    if name not in ("condition", "timeframe")
                     },
                 condition=condition,
                 source=source,

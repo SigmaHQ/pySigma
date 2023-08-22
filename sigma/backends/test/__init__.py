070d2cf7dc3b0c3159181df5c59ae16c3e4049ef
Thomas Patzke <thomas@patzke.org>
2023-08-22 23:58:04 +0200

    Removal of backend and pipeline directories

--- sigma/backends/test/__init__.py
+++ sigma/backends/test/__init__.py
@@ -1,9 +1,1 @@
 from .backend import TextQueryTestBackend, MandatoryPipelineTestBackend
-import sys
-import os
-
-if "pytest" in sys.modules:
-    backends = {
-        "test": TextQueryTestBackend,
-        "test_mandatory": MandatoryPipelineTestBackend,
-    }
--- sigma/pipelines/test/__init__.py
+++ sigma/pipelines/test/__init__.py
@@ -2,9 +2,3 @@ import sys
 from .pipeline import dummy_test_pipeline, another_test_pipeline, YetAnotherTestPipeline
 
 __all__ = ["YetAnotherTestPipeline"]
-
-if "pytest" in sys.modules:
-    pipelines = {
-        "test": dummy_test_pipeline,
-        "another_test": another_test_pipeline,
-    }

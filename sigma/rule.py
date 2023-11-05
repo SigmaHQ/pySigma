6ec8b38b0cacd9687dc1024813e3344f97edde15
frack113 <62423083+frack113@users.noreply.github.com>
2023-11-05 15:22:27 +0100

    Add modified to SigmaRule Class

--- sigma/rule.py
+++ sigma/rule.py
@@ -611,6 +611,7 @@ class SigmaRule(ProcessingItemTrackingMixin):
     tags: List[SigmaRuleTag] = field(default_factory=list)
     author: Optional[str] = None
     date: Optional[date] = None
+    modified: Optional[date] = None
     fields: List[str] = field(default_factory=list)
     falsepositives: List[str] = field(default_factory=list)
     level: Optional[SigmaLevel] = None
@@ -701,6 +702,23 @@ class SigmaRule(ProcessingItemTrackingMixin):
                         errors.append(
                             sigma_exceptions.SigmaDateError(
                                 f"Rule date '{ rule_date }' is invalid, must be yyyy/mm/dd or yyyy-mm-dd",
+                                source=source,
+                            )
+                        )
+
+        # parse rule modified if existing
+        rule_modified = rule.get("modified")
+        if rule_modified is not None:
+            if not isinstance(rule_modified, date) and not isinstance(rule_modified, datetime):
+                try:
+                    rule_modified = date(*(int(i) for i in rule_modified.split("/")))
+                except ValueError:
+                    try:
+                        rule_modified = date(*(int(i) for i in rule_modified.split("-")))
+                    except ValueError:
+                        errors.append(
+                            sigma_exceptions.SigmaDateError(
+                                f"Rule modified '{ rule_modified }' is invalid, must be yyyy/mm/dd or yyyy-mm-dd",
                                 source=source,
                             )
                         )

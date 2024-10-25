25658ccc4ff31fbf83b38316e06a8e1adfd11f4c
Thomas Patzke <thomas@patzke.org>
2024-10-26 01:09:02 +0200

    Splitted rule transformations

--- sigma/processing/transformations/rule.py
+++ sigma/processing/transformations/rule.py
@@ -10,14 +10,9 @@ from dataclasses import dataclass, field
 import sigma
 from sigma.correlations import SigmaCorrelationRule
 from sigma.processing.transformations.base import (
-    DetectionItemTransformation,
     Transformation,
 )
-from sigma.rule import SigmaLogSource, SigmaRule, SigmaDetectionItem
-from sigma.exceptions import (
-    SigmaTransformationError,
-    SigmaConfigurationError,
-)
+from sigma.rule import SigmaLogSource, SigmaRule
 
 
 @dataclass
@@ -37,48 +32,6 @@ class ChangeLogsourceTransformation(Transformation):
 
 
 @dataclass
-class SetStateTransformation(Transformation):
-    """Set pipeline state key to value."""
-
-    key: str
-    val: Any
-
-    def apply(self, pipeline: "sigma.processing.pipeline.Proces", rule: SigmaRule) -> None:
-        super().apply(pipeline, rule)
-        pipeline.state[self.key] = self.val
-
-
-@dataclass
-class RuleFailureTransformation(Transformation):
-    """
-    Raise a SigmaTransformationError with the provided message. This enables transformation
-    pipelines to signalize that a certain situation can't be handled, e.g. only a subset of values
-    is allowed because the target data model doesn't offers all possibilities.
-    """
-
-    message: str
-
-    def apply(
-        self, pipeline: "sigma.processing.pipeline.ProcessingPipeline", rule: SigmaRule
-    ) -> None:
-        raise SigmaTransformationError(self.message)
-
-
-@dataclass
-class DetectionItemFailureTransformation(DetectionItemTransformation):
-    """
-    Raise a SigmaTransformationError with the provided message. This enables transformation
-    pipelines to signalize that a certain situation can't be handled, e.g. only a subset of values
-    is allowed because the target data model doesn't offers all possibilities.
-    """
-
-    message: str
-
-    def apply_detection_item(self, detection_item: SigmaDetectionItem) -> None:
-        raise SigmaTransformationError(self.message)
-
-
-@dataclass
 class SetCustomAttributeTransformation(Transformation):
     """
     Sets an arbitrary custom attribute on a rule, that can be used by a backend during processing.
@@ -94,48 +47,3 @@ class SetCustomAttributeTransformation(Transformation):
     ) -> None:
         super().apply(pipeline, rule)
         rule.custom_attributes[self.attribute] = self.value
-
-
-@dataclass
-class NestedProcessingTransformation(Transformation):
-    """Executes a nested processing pipeline as transformation. Main purpose is to apply a
-    whole set of transformations that match the given conditions of the enclosng processing item.
-    """
-
-    items: List["sigma.processing.pipeline.ProcessingItem"]
-    _nested_pipeline: "sigma.processing.pipeline.ProcessingPipeline" = field(
-        init=False, compare=False, repr=False
-    )
-
-    def __post_init__(self):
-        from sigma.processing.pipeline import (
-            ProcessingPipeline,
-        )  # TODO: move to top-level after restructuring code
-
-        self._nested_pipeline = ProcessingPipeline(items=self.items)
-
-    @classmethod
-    def from_dict(cls, d: Dict) -> "NestedProcessingTransformation":
-        from sigma.processing.pipeline import (
-            ProcessingItem,
-        )  # TODO: move to top-level after restructuring code
-
-        try:
-            return cls(items=[ProcessingItem.from_dict(item) for item in d["items"]])
-        except KeyError:
-            raise SigmaConfigurationError(
-                "Nested processing transformation requires an 'items' key."
-            )
-
-    def apply(
-        self,
-        pipeline: "sigma.processing.pipeline.ProcessingPipeline",
-        rule: Union[SigmaRule, SigmaCorrelationRule],
-    ) -> None:
-        super().apply(pipeline, rule)
-        self._nested_pipeline.apply(rule)
-        pipeline.applied.extend(self._nested_pipeline.applied)
-        pipeline.applied_ids.update(self._nested_pipeline.applied_ids)
-        pipeline.field_name_applied_ids.update(self._nested_pipeline.field_name_applied_ids)
-        pipeline.field_mappings.merge(self._nested_pipeline.field_mappings)
-        pipeline.state.update(self._nested_pipeline.state)

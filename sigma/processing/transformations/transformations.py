0ce4d6dc11836ff2185e62bdf41147ae77f83851
Thomas Patzke <thomas@patzke.org>
2024-10-26 01:11:06 +0200

    Removed splitted rule transformations

--- sigma/processing/transformations/transformations.py
+++ sigma/processing/transformations/transformations.py
@@ -1,9 +1,7 @@
-from abc import abstractmethod
 from typing import (
     Any,
     List,
     Dict,
-    Optional,
     Union,
 )
 from dataclasses import dataclass, field
@@ -13,27 +11,11 @@ from sigma.processing.transformations.base import (
     DetectionItemTransformation,
     Transformation,
 )
-from sigma.rule import SigmaLogSource, SigmaRule, SigmaDetectionItem
+from sigma.rule import SigmaRule, SigmaDetectionItem
 from sigma.exceptions import (
     SigmaTransformationError,
     SigmaConfigurationError,
 )
-
-
-@dataclass
-class ChangeLogsourceTransformation(Transformation):
-    """Replace log source as defined in transformation parameters."""
-
-    category: Optional[str] = field(default=None)
-    product: Optional[str] = field(default=None)
-    service: Optional[str] = field(default=None)
-
-    def apply(
-        self, pipeline: "sigma.processing.pipeline.ProcessingPipeline", rule: SigmaRule
-    ) -> None:
-        super().apply(pipeline, rule)
-        logsource = SigmaLogSource(self.category, self.product, self.service)
-        rule.logsource = logsource
 
 
 @dataclass
@@ -76,24 +58,6 @@ class DetectionItemFailureTransformation(DetectionItemTransformation):
 
     def apply_detection_item(self, detection_item: SigmaDetectionItem) -> None:
         raise SigmaTransformationError(self.message)
-
-
-@dataclass
-class SetCustomAttributeTransformation(Transformation):
-    """
-    Sets an arbitrary custom attribute on a rule, that can be used by a backend during processing.
-    """
-
-    attribute: str
-    value: Any
-
-    def apply(
-        self,
-        pipeline: "sigma.processing.pipeline.ProcessingPipeline",
-        rule: Union[SigmaRule, SigmaCorrelationRule],
-    ) -> None:
-        super().apply(pipeline, rule)
-        rule.custom_attributes[self.attribute] = self.value
 
 
 @dataclass

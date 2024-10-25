2e71215eb5767a7140b42633fe65d9d240964524
Thomas Patzke <thomas@patzke.org>
2024-10-26 01:01:28 +0200

    Remove splitted condition transformation

--- sigma/processing/transformations/transformations.py
+++ sigma/processing/transformations/transformations.py
@@ -1,5 +1,4 @@
 from abc import abstractmethod
-from sigma.conditions import SigmaCondition
 from typing import (
     Any,
     List,
@@ -8,75 +7,17 @@ from typing import (
     Union,
 )
 from dataclasses import dataclass, field
-import random
-import string
 import sigma
 from sigma.correlations import SigmaCorrelationRule
 from sigma.processing.transformations.base import (
-    ConditionTransformation,
     DetectionItemTransformation,
     Transformation,
 )
-from sigma.rule import SigmaLogSource, SigmaRule, SigmaDetection, SigmaDetectionItem
+from sigma.rule import SigmaLogSource, SigmaRule, SigmaDetectionItem
 from sigma.exceptions import (
     SigmaTransformationError,
     SigmaConfigurationError,
 )
-
-
-@dataclass
-class AddConditionTransformation(ConditionTransformation):
-    """
-    Add a condition expression to rule conditions.
-
-    If template is set to True the condition values are interpreted as string templates and the
-    following placeholders are replaced:
-
-    * $category, $product and $service: with the corresponding values of the Sigma rule log source.
-    """
-
-    conditions: Dict[str, Union[str, List[str]]] = field(default_factory=dict)
-    name: Optional[str] = field(default=None, compare=False)
-    template: bool = False
-    negated: bool = False
-
-    def __post_init__(self):
-        if self.name is None:  # generate random detection item name if none is given
-            self.name = "_cond_" + ("".join(random.choices(string.ascii_lowercase, k=10)))
-
-    def apply(
-        self, pipeline: "sigma.processing.pipeline.ProcessingPipeline", rule: SigmaRule
-    ) -> None:
-        if isinstance(rule, SigmaRule):
-            if self.template:
-                conditions = {
-                    field: (
-                        [
-                            string.Template(item).safe_substitute(
-                                category=rule.logsource.category,
-                                product=rule.logsource.product,
-                                service=rule.logsource.service,
-                            )
-                            for item in value
-                        ]
-                        if isinstance(value, list)
-                        else string.Template(value).safe_substitute(
-                            category=rule.logsource.category,
-                            product=rule.logsource.product,
-                            service=rule.logsource.service,
-                        )
-                    )
-                    for field, value in self.conditions.items()
-                }
-            else:
-                conditions = self.conditions
-
-            rule.detection.detections[self.name] = SigmaDetection.from_definition(conditions)
-            self.processing_item_applied(rule.detection.detections[self.name])
-            super().apply(pipeline, rule)
-
-    def apply_condition(self, cond: SigmaCondition) -> None:
-        cond.condition = ("not " if self.negated else "") + f"{self.name} and ({cond.condition})"
 
 
 @dataclass

13e6b03a6f3e5a657f7ff77040aec91e6c43ae01
Thomas Patzke <thomas@patzke.org>
2024-10-28 22:57:20 +0100

    Splitted value conditions

--- sigma/processing/conditions/values.py
+++ sigma/processing/conditions/values.py
@@ -2,12 +2,9 @@ from dataclasses import dataclass
 
 import sigma
 from sigma.processing.conditions.base import (
-    DetectionItemProcessingCondition,
-    RuleProcessingCondition,
     ValueProcessingCondition,
 )
 from sigma.types import SigmaNull, SigmaString, SigmaType
-from typing import Dict
 import re
 from sigma.exceptions import SigmaRegularExpressionError
 
@@ -72,31 +69,3 @@ class IsNullCondition(ValueProcessingCondition):
         self, pipeline: "sigma.processing.pipeline.ProcessingPipeline", value: SigmaType
     ) -> bool:
         return isinstance(value, SigmaNull)
-
-
-### Condition mappings between rule identifier and class
-
-rule_conditions: Dict[str, RuleProcessingCondition] = {
-    "logsource": LogsourceCondition,
-    "contains_detection_item": RuleContainsDetectionItemCondition,
-    "contains_field": RuleContainsFieldCondition,
-    "processing_item_applied": RuleProcessingItemAppliedCondition,
-    "processing_state": RuleProcessingStateCondition,
-    "is_sigma_rule": IsSigmaRuleCondition,
-    "is_sigma_correlation_rule": IsSigmaCorrelationRuleCondition,
-    "rule_attribute": RuleAttributeCondition,
-    "tag": RuleTagCondition,
-}
-detection_item_conditions: Dict[str, DetectionItemProcessingCondition] = {
-    "match_string": MatchStringCondition,
-    "contains_wildcard": ContainsWildcardCondition,
-    "is_null": IsNullCondition,
-    "processing_item_applied": DetectionItemProcessingItemAppliedCondition,
-    "processing_state": DetectionItemProcessingStateCondition,
-}
-field_name_conditions: Dict[str, DetectionItemProcessingCondition] = {
-    "include_fields": IncludeFieldCondition,
-    "exclude_fields": ExcludeFieldCondition,
-    "processing_item_applied": FieldNameProcessingItemAppliedCondition,
-    "processing_state": FieldNameProcessingStateCondition,
-}

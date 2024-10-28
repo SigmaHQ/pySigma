ba27b2a58894a14573e2c5c9964c8a2ea5e93c71
Thomas Patzke <thomas@patzke.org>
2024-10-28 22:50:28 +0100

    Remove splitted state conditions

--- sigma/processing/conditions/conditions.py
+++ sigma/processing/conditions/conditions.py
@@ -1,94 +1,17 @@
 from dataclasses import dataclass, field
-from datetime import date
-from uuid import UUID
 
 import sigma
-from sigma.correlations import SigmaCorrelationRule
 from sigma.processing.conditions.base import (
     DetectionItemProcessingCondition,
     FieldNameProcessingCondition,
-    RuleDetectionItemCondition,
     RuleProcessingCondition,
     ValueProcessingCondition,
 )
-from sigma.types import SigmaNull, SigmaString, SigmaType, sigma_type
-from typing import ClassVar, Dict, List, Pattern, Literal, Optional, Union
+from sigma.types import SigmaNull, SigmaString, SigmaType
+from typing import Dict, List, Pattern, Literal, Optional
 import re
-from sigma.rule import (
-    SigmaDetection,
-    SigmaLevel,
-    SigmaRule,
-    SigmaDetectionItem,
-    SigmaLogSource,
-    SigmaRuleTag,
-    SigmaStatus,
-)
+from sigma.rule import SigmaDetectionItem
 from sigma.exceptions import SigmaConfigurationError, SigmaRegularExpressionError
-
-
-@dataclass
-class RuleProcessingItemAppliedCondition(RuleProcessingCondition):
-    """
-    Checks if processing item was applied to rule.
-    """
-
-    processing_item_id: str
-
-    def match(
-        self,
-        pipeline: "sigma.processing.pipeline.ProcessingPipeline",
-        rule: Union[SigmaRule, SigmaCorrelationRule],
-    ) -> bool:
-        return rule.was_processed_by(self.processing_item_id)
-
-
-@dataclass
-class ProcessingStateConditionBase:
-    """
-    Base class for processing pipeline state matching. The method match_state can be used by the
-    matching method of the derived condition classes to match the state condition.
-    """
-
-    key: str
-    val: Union[str, int, float, bool]
-    op: Literal["eq", "ne", "gte", "gt", "lte", "lt"] = field(default="eq")
-
-    def match_state(self, pipeline: "sigma.processing.pipeline.ProcessingPipeline") -> bool:
-        try:
-            state_val = pipeline.state[self.key]
-        except KeyError:
-            return False
-
-        if self.op == "eq":
-            return state_val == self.val
-        elif self.op == "ne":
-            return state_val != self.val
-        elif self.op == "gte":
-            return state_val >= self.val
-        elif self.op == "gt":
-            return state_val > self.val
-        elif self.op == "lte":
-            return state_val <= self.val
-        elif self.op == "lt":
-            return state_val < self.val
-        else:
-            raise SigmaConfigurationError(
-                f"Invalid operation '{self.op}' in rule state condition {str(self)}."
-            )
-
-
-@dataclass
-class RuleProcessingStateCondition(RuleProcessingCondition, ProcessingStateConditionBase):
-    """
-    Matches on processing pipeline state.
-    """
-
-    def match(
-        self,
-        pipeline: "sigma.processing.pipeline.ProcessingPipeline",
-        rule: Union[SigmaRule, SigmaCorrelationRule],
-    ) -> bool:
-        return self.match_state(pipeline)
 
 
 @dataclass
@@ -146,20 +69,6 @@ class ExcludeFieldCondition(IncludeFieldCondition):
         detection_item: SigmaDetectionItem,
     ) -> bool:
         return not super().match_field_name(pipeline, detection_item)
-
-
-@dataclass
-class FieldNameProcessingStateCondition(FieldNameProcessingCondition, ProcessingStateConditionBase):
-    """
-    Matches on processing pipeline state in context of a field name condition.
-    """
-
-    def match_field_name(
-        self,
-        pipeline: "sigma.processing.pipeline.ProcessingPipeline",
-        field: str,
-    ) -> bool:
-        return self.match_state(pipeline)
 
 
 ### Detection Item Condition Classes ###
@@ -223,59 +132,6 @@ class IsNullCondition(ValueProcessingCondition):
         self, pipeline: "sigma.processing.pipeline.ProcessingPipeline", value: SigmaType
     ) -> bool:
         return isinstance(value, SigmaNull)
-
-
-@dataclass
-class DetectionItemProcessingItemAppliedCondition(DetectionItemProcessingCondition):
-    """
-    Checks if processing item was applied to detection item.
-    """
-
-    processing_item_id: str
-
-    def match(
-        self,
-        pipeline: "sigma.processing.pipeline.ProcessingPipeline",
-        detection_item: SigmaDetectionItem,
-    ) -> bool:
-        return detection_item.was_processed_by(self.processing_item_id)
-
-
-@dataclass
-class DetectionItemProcessingStateCondition(
-    DetectionItemProcessingCondition, ProcessingStateConditionBase
-):
-    """
-    Matches on processing pipeline state in context of a detection item condition.
-    """
-
-    def match(
-        self,
-        pipeline: "sigma.processing.pipeline.ProcessingPipeline",
-        detection_item: SigmaDetectionItem,
-    ) -> bool:
-        return self.match_state(pipeline)
-
-
-@dataclass
-class FieldNameProcessingItemAppliedCondition(FieldNameProcessingCondition):
-    """
-    Checks if processing item was applied to a field name.
-    """
-
-    processing_item_id: str
-
-    def match_field_name(
-        self, pipeline: "sigma.processing.pipeline.ProcessingPipeline", field: str
-    ) -> bool:
-        return pipeline.field_was_processed_by(field, self.processing_item_id)
-
-    def match_detection_item(
-        self,
-        pipeline: "sigma.processing.pipeline.ProcessingPipeline",
-        detection_item: SigmaDetectionItem,
-    ):
-        return detection_item.was_processed_by(self.processing_item_id)
 
 
 ### Condition mappings between rule identifier and class
